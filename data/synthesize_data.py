d3b668dea5c6953af220662199411897ddc3d917
almajd3713 <almajd3713@gmail.com>
2025-07-16 10:38:31 +0400

    more work on synthesis

--- data/synthesize_data.py
+++ data/synthesize_data.py
@@ -13,8 +13,20 @@ if not os.path.exists(os.path.join(base_dir, "logs")):
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', filename=log_filename, filemode='w')
 #! --------------
 
+def clear_directory(directory):
+    if os.path.exists(directory):
+        for filename in os.listdir(directory):
+            file_path = os.path.join(directory, filename)
+            try:
+                if os.path.isfile(file_path) or os.path.islink(file_path):
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path):
+                    os.rmdir(file_path)
+            except Exception as e:
+                logging.error(f"Failed to delete {file_path}. Reason: {e}")
+
 class DataSynthesizer:
-  def __init__(self, data_file, backgrounds_file, max_frequency=25):
+  def __init__(self, data_file, backgrounds_file, max_frequency=100):
     self.data_file = data_file
     self.backgrounds_file = backgrounds_file
     self.classes, self.image_data = self.load_data()
@@ -53,14 +65,20 @@ class DataSynthesizer:
       scale_factor = 4
       image = cv2.resize(image, (w * scale_factor, h * scale_factor), interpolation=cv2.INTER_NEAREST)
     return image
-  
-  def is_underfilled(self):
+
+  def get_used_classes(self):
+    not_full_classes = [classs for classs in self.classes if classs[1] < self.max_frequency]
+    most_frequent = max(not_full_classes, key=lambda x: x[1])
+    least_frequent = min(not_full_classes, key=lambda x: x[1])
+    return most_frequent, least_frequent
+
+  def get_underfilled_classes(self):
     underfilled_classes = [classs for classs, freq in self.classes if freq < self.max_frequency]
-    return len(underfilled_classes) > 0
+    return underfilled_classes
 
   def load_classes_sample(self, max_sample_size=5):
-    underfilled_classes = [classs for classs, freq in self.classes if freq < self.max_frequency]
-    sample_classes = random.sample(underfilled_classes, random.randint(1, max_sample_size))
+    sample_classes = self.get_underfilled_classes()
+    sample_classes = random.sample(sample_classes, random.randint(1, max_sample_size))
     sample = [it for it in self.image_data if it['class_name'] in sample_classes]
     return sample, [self.load_image(image['url'], image['name'], scale=True) for image in sample]
 
@@ -104,13 +122,16 @@ class DataSynthesizer:
   
   def synthesize(self):
     image_data_list = []
-    background_info, background = self.load_background_sample()
+    _, background = self.load_background_sample()
     classes, foregrounds = self.load_classes_sample()
     image_name = uuid.uuid4().hex
     image_path = os.path.join(base_dir, "synthesized_images", f"{image_name}.png")
     # synthesized_image, bbox = self.overlay_images(foreground, background)
     final_image = background.copy()
     for image_data, foreground in zip(classes, foregrounds):
+      if foreground is None:
+        print(image_data)
+      self.update_frequency(image_data['class_name'])
       synthesized_image, bbox = self.overlay_images(foreground, final_image)
       if synthesized_image is None:
         logging.error("Failed to synthesize image. Skipping.")
@@ -126,12 +147,16 @@ class DataSynthesizer:
       with open(image_path, "wb") as f:
         cv2.imwrite(f.name, final_image)
       logging.info(f"Synthesized and saved image {image_name}")
-      self.update_frequency(image_data['class_name'])
     return image_data_list
   
   def synthesize_until_satisfied(self):
     image_data_list = []
-    while self.is_underfilled():
+    while len(self.get_underfilled_classes()):
+      underfilled_length = len(self.get_underfilled_classes())
+      most_frequent, least_frequent = self.get_used_classes()
+      logging.info(f"Underfilled classes: {underfilled_length}")
+      logging.info(f"Most frequent class: {most_frequent}")
+      logging.info(f"Least frequent class: {least_frequent}")
       image_data_list.extend(self.synthesize())
     return image_data_list
 
@@ -158,7 +183,9 @@ if __name__ == "__main__":
   logging.info(f'Synthesizing images with {len(sample_images)} sample images and background {sample_backgrounds[0]["filename"]}')
   if not os.path.exists(os.path.join(base_dir, "synthesized_images")):
     os.makedirs(os.path.join(base_dir, "synthesized_images"))
-  synthesized_data = synthesizer.synthesize()
+  clear_directory(os.path.join(base_dir, "synthesized_images"))
+  synthesized_data = synthesizer.synthesize_until_satisfied()
   with open(os.path.join(base_dir, "synthesized_images", "synthesized_data.json"), 'w') as f:
     json.dump(synthesized_data, f, indent=4)
-  logging.info(f'Finished synthesizing images. Generated {len(synthesized_data)} images.')
+  final_image_count = len(os.listdir(os.path.join(base_dir, "synthesized_images"))) - 1  # -1 for the synthesized_data.json file
+  logging.info(f'Finished synthesizing images. Generated {len(synthesized_data)} items in {final_image_count} images.')
